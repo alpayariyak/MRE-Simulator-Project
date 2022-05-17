f51597e341a6e4ed1e26469421fdda655011edaa
alpayariyak <98838263+alpayariyak@users.noreply.github.com>
2022-05-17 16:31:06 -0400

    simulator, in progress

--- simulator.py
+++ simulator.py
@@ -1,28 +1,43 @@
 import math
 
 # page height, easier to reference js code
+import random
+
 cnvheight = 1230  # or 800
 cnvwidth = 3024
 
 #
 totalObjects = -11
 totalRejects = 0
-detectedRejects = 0
-detectedFp = 0
+trash_id = 0
 #
-trash_classes = ['aluminumCan',
-                 'cardboard',
-                 'carton',
-                 'glassBottle',
-                 'paper',
-                 'paperBag',
-                 'plasticBag',
-                 'reject']
+
+aluminumCan = [f'aluminumCan/can{i}.png' for i in range(10)]
+cardboard = [f'cardboard/cardboard{i}.png' for i in range(10)]
+carton = [f'carton/carton{i}.png' for i in range(10)]
+glassBottle = [f'glassBottle/glassBottle{i}.png' for i in range(9)]
+paper = [f'paper/paper{i}.png' for i in range(21)]
+paperBag = [f'paperBag/paperBag{i}.png' for i in range(15)]
+plasticBag = [f'plasticBag/plasticBag{i}.png' for i in range(23)]
+plasticBottle = [f'plasticBottle/plasticBottle{i}.png' for i in range(12)]
+reject = [f'reject/reject{i}.png' for i in range(20)]
+
+trash_classes = aluminumCan \
+                + cardboard \
+                + carton \
+                + glassBottle \
+                + paper \
+                + paperBag \
+                + plasticBag \
+                + plasticBottle \
+                + reject
 
 
 class Simulator_Object:
-    def __init__(self, obj_class, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
-        self.obj_class = obj_class
+
+    def __init__(self, object, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
+        self.obj_class = object.split('/')[0]
+        self.object = object
         self.rot = (rot * math.pi) / 180
         self.height = height
         self.width = width
@@ -30,7 +45,11 @@ class Simulator_Object:
         self.speedy = speedy
         self.x = x
         self.y = y
-        self.state = [x, y, width, height, speedx, speedy, rot]
+        self.state = [x, y, speedx, speedy]
+
+        global totalObjects
+        global totalRejects
+        totalObjects += 1
 
 
 # might not need these
@@ -73,8 +92,68 @@ belt3 = Simulator_Object('ConvBeltNew',
                          width=cnvwidth + 150,  # width
                          height=160)  # height
 
+trash_bin = Simulator_Object('trashbin',
+                             cnvwidth / 2 - 75,
+                             15,
+                             width=150,
+                             height=150)
+
 belts = [belt1, belt2, belt3]
 endboxes = [end1, end2, end3]
 
-import js2py
-js2py.translate_file('MRE-Simulator-Project/conveyer-stripped.js', 'MRE-Simulator-Project/translated.py')
+trash_objects = []
+
+
+class Trash_Object(Simulator_Object):
+    def __init__(self, object, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
+        global trash_id
+        trash_id = trash_id+1
+        super().__init__(object, x, y, speedx, speedy, rot, width, height)
+
+
+        global totalRejects
+        if self.obj_class == 'reject':
+            totalRejects += 1
+
+
+print(len(trash_classes))
+
+
+def makeRandomTrash(beltNumber):
+    if beltNumber == 1:
+        globals()[f'trash_object_{trash_id}'] = Trash_Object(
+            random.choice(trash_classes),
+            -100 - random.randint(0, 150),
+            200,
+            speedx=3,
+            speedy=0,
+            rot=random.randint(-90, 90)
+        )
+
+    elif beltNumber == 2:
+        globals()[f'trash_object_{trash_id}'] = Trash_Object(
+            random.choice(trash_classes),
+            -100 - random.randint(0, 150),
+            400,
+            speedx=2,
+            speedy=0,
+            rot=random.randint(-90, 90)
+        )
+    else:
+        globals()[f'trash_object_{trash_id}'] = Trash_Object(
+            random.choice(trash_classes),
+            -100 - random.randint(0, 150),
+            600,
+            speedx=4,
+            speedy=0,
+            rot=random.randint(-90, 90)
+        )
+
+makeRandomTrash(1)
+makeRandomTrash(1)
+makeRandomTrash(1)
+makeRandomTrash(1)
+makeRandomTrash(1)
+makeRandomTrash(1)
+
+print(totalRejects)
