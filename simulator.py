058d95661a6ed137c6ab2c9afa6a478306196fae
alpayariyak <98838263+alpayariyak@users.noreply.github.com>
2022-05-18 23:01:01 -0400

    a lot
    replaced default operands for simulator objects to allow for checking of intersection of another sim object or a mouse position, made a belt class to prepare items to change speed once they are on a belt automatically and more i cant remember

--- simulator.py
+++ simulator.py
@@ -2,6 +2,7 @@ import math
 
 # page height, easier to reference js code
 import random
+import math
 
 cnvheight = 1230  # or 800
 cnvwidth = 3024
@@ -35,9 +36,9 @@ trash_classes = aluminumCan \
 
 class Simulator_Object:
 
-    def __init__(self, object, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
-        self.obj_class = object.split('/')[0]
-        self.object = object
+    def __init__(self, obj, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
+        self.obj_class = obj.split('/')[0]
+        self.object = obj
         self.rot = (rot * math.pi) / 180
         self.height = height
         self.width = width
@@ -45,13 +46,64 @@ class Simulator_Object:
         self.speedy = speedy
         self.x = x
         self.y = y
-
+        self.hitbox = {"x": int(x + width / 2), "y": int(y + height / 2), "radius": 50}
 
         global totalObjects
         global totalRejects
         totalObjects += 1
+
     def get_state(self):
         return [self.x, self.y, self.speedx, self.speedy]
+
+    def __contains__(self, item):
+
+        xcenter_self, ycenter_self, radius = self.hitbox["x"], self.hitbox["y"], self.hitbox["radius"]
+
+        if type(item) is Simulator_Object or Trash_Object:
+
+            xcenter_item, ycenter_item = item.hitbox["x"], item.hitbox["y"]
+
+            if self.x + self.width > xcenter_item > self.x and self.y + self.height > ycenter_item > self.y:
+                return True
+            else:
+                return False
+
+        if type(item) is list:
+
+            x_item, y_item = item
+            dist = math.sqrt(
+                math.pow(abs(xcenter_self - x_item), 2) + math.pow(abs(ycenter_self - y_item), 2)
+            )
+
+            if dist < radius:
+                return True
+            else:
+                return False
+
+
+class Belt(Simulator_Object):
+    def __init__(self, belt_number,
+                 x=-50,
+                 speedx=0,
+                 speedy=0,
+                 rot=0,
+                 width=cnvwidth + 150,
+                 height=160,
+                 obj='ConvBeltNew'):
+
+        belt_speeds = [3, 2, 4]
+        if belt_number == 1:
+            self.y = 185
+            self.belt_speed = belt_speeds[0]
+        elif belt_number == 2:
+            self.y = 385
+            self.belt_speed = belt_speeds[1]
+        elif belt_number == 3:
+            self.y = 585
+            self.belt_speed = belt_speeds[2]
+
+        super().__init__(obj, x, self.y, speedx, speedy, rot, width, height)
+
 
 # might not need these
 # endboxes
@@ -75,23 +127,11 @@ end3 = Simulator_Object('undergroundConvBelt',
 
 # belts
 
-belt1 = Simulator_Object('ConvBeltNew',
-                         -50,  # x
-                         185,  # y
-                         width=cnvwidth + 150,  # width
-                         height=160)  # height
+belt1 = Belt(1)
 
-belt2 = Simulator_Object('ConvBeltNew',
-                         -50,  # x
-                         385,  # y
-                         width=cnvwidth + 150,  # width
-                         height=160)  # height
+belt2 = Belt(2)
 
-belt3 = Simulator_Object('ConvBeltNew',
-                         -50,  # x
-                         585,  # y
-                         width=cnvwidth + 150,  # width
-                         height=160)  # height
+belt3 = Belt(3)
 
 trash_bin = Simulator_Object('trashbin',
                              cnvwidth / 2 - 75,
@@ -102,19 +142,19 @@ trash_bin = Simulator_Object('trashbin',
 belts = [belt1, belt2, belt3]
 endboxes = [end1, end2, end3]
 
-trash_objects = {} #id:object
+trash_objects = {}  # id:object
 
 
 class Trash_Object(Simulator_Object):
-    def __init__(self, object, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
+    def __init__(self, obj, x, y, speedx=0, speedy=0, rot=0, width=100, height=100):
         global trash_id
-        trash_id = trash_id+1
-        super().__init__(object, x, y, speedx, speedy, rot, width, height)
-
+        trash_id = trash_id + 1
+        super().__init__(obj, x, y, speedx, speedy, rot, width, height)
 
         global totalRejects
         if self.obj_class == 'reject':
             totalRejects += 1
+
     def update_position(self):
         self.x = self.x + self.speedx
         self.y = self.y + self.speedy
@@ -124,16 +164,13 @@ class Trash_Object(Simulator_Object):
         self.y = new_y
 
 
-print(len(trash_classes))
-
-
 def makeRandomTrash(beltNumber):
     if beltNumber == 1:
         globals()[f'trash_object_{trash_id}'] = Trash_Object(
             random.choice(trash_classes),
             -100 - random.randint(0, 150),
             200,
-            speedx=3,
+            speedx=belt1.belt_speed,
             speedy=0,
             rot=random.randint(-90, 90)
         )
@@ -143,7 +180,7 @@ def makeRandomTrash(beltNumber):
             random.choice(trash_classes),
             -100 - random.randint(0, 150),
             400,
-            speedx=2,
+            speedx=belt2.belt_speed,
             speedy=0,
             rot=random.randint(-90, 90)
         )
@@ -153,7 +190,7 @@ def makeRandomTrash(beltNumber):
             random.choice(trash_classes),
             -100 - random.randint(0, 150),
             600,
-            speedx=4,
+            speedx=belt3.belt_speed,
             speedy=0,
             rot=random.randint(-90, 90)
         )
@@ -162,14 +199,37 @@ def makeRandomTrash(beltNumber):
 makeRandomTrash(1)
 makeRandomTrash(2)
 makeRandomTrash(3)
-makeRandomTrash(2)
-makeRandomTrash(3)
-makeRandomTrash(2)
+
 
 for i in range(5):
     for id, trash in trash_objects.items():
         trash.update_position()
         if i % 1 == 0:
-            print(f"TrashID: {id}  State: {trash.get_state()}   (x, y, speedx, speedy)")
-        if id == 6 and i % 1 == 0:
+            print(f"TrashID: {id}  State: {trash.get_state()}")
+        if id == 3 and i % 1 == 0:
             print("\n")
+
+score = 0
+
+# for i in range(180000): #180000 ms in 3 minutes
+#     if i % 50:
+#         makeRandomTrash(1)
+#         makeRandomTrash(2)
+#         makeRandomTrash(3)
+#     if i % 10:
+
+
+xcenter_self, ycenter_self, radius = trash_bin.hitbox["x"], trash_bin.hitbox["y"], trash_bin.hitbox["radius"]
+
+print(xcenter_self, ycenter_self, radius)
+
+randomtr = Trash_Object(
+    'aluminumCan',
+    1512,
+    90,
+    speedx=0,
+    speedy=0,
+    rot=random.randint(-90, 90)
+)
+
+print(randomtr in trash_bin)
