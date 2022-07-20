618886017d988f6512b1eb1b46198b55dc31bba4
alpayariyak <98838263+alpayariyak@users.noreply.github.com>
2022-07-20 16:45:43 -0400

    minibatch

--- src/RL_v2.py
+++ src/RL_v2.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 import numpy as np
 from itertools import count
-
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -43,6 +43,9 @@ class Policy(nn.Module):
         self.saved_log_probs = []
         self.rewards = []
 
+        self.total_saved_log_probs = []
+        self.total_rewards = []
+
     def forward(self, x):
         x = self.affine1(x)
         x = self.dropout(x)
@@ -61,21 +64,23 @@ def select_action(state, in_policy):
 
 
 def finish_episode():
-    total_policy_loss = torch.empty()
-    
-    R = 0
-    policy_loss = []
-    returns = []  # sum of rewards
-    for r in training_policy.rewards[::-1]:
-        R = r + args.gamma * R  # gamma = 1
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-    for log_prob, R in zip(training_policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
+    total_policy_loss = torch.empty(())
+    for run in range(len(training_policy.total_saved_log_probs)):
+        R = 0
+        policy_loss = []
+        returns = []  # sum of rewards
+        for r in training_policy.total_rewards[run][::-1]:
+            R = r + args.gamma * R  # gamma = 1
+            returns.insert(0, R)
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + eps)
+        for log_prob, R in zip(training_policy.total_saved_log_probs[run], returns):
+            policy_loss.append(-log_prob * R)
+        policy_loss = torch.cat(policy_loss).sum()
+        total_policy_loss += policy_loss/5
+
     optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
+    total_policy_loss.backward()
     optimizer.step()
 
     del training_policy.rewards[:]
@@ -114,14 +119,18 @@ def main():
     # running_reward = av
     # print(av)
 
-    for i_episode in range(100000):
-        A, X, total_reward, rewards_H, score = simulator(training_policy, 5)
-        training_policy.rewards = rewards_H
+    for i_episode in range(3):
+        training_policy.total_saved_log_probs = [[] for _ in range(3)]
+        for n in range(3):
+            A, X, total_reward, rewards_H, score = simulator(training_policy, 5)
+            training_policy.total_rewards.append(rewards_H)
+            training_policy.total_saved_log_probs[n] = training_policy.saved_log_probs
+            training_policy.saved_log_probs = []
 
         running_reward = 0.05 * total_reward + (1 - 0.05) * running_reward
         finish_episode()
 
-        if i_episode % 100 == 0:
+        if i_episode % 25 == 0:
             print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f} \t Score: {}'.format(
                 i_episode, total_reward, running_reward, score))
             a_temp = [np.argmax(a_) for a_ in A]
@@ -139,4 +148,10 @@ def main():
             print(avg / 30)
 
 if __name__ == '__main__':
+
+
+    start_time = time.time()
+
     main()
+    # # trained_theta = train(5, 30, 0.3)
+    print("--- %s seconds ---" % (time.time() - start_time))
--- src/global_.py
+++ src/global_.py
@@ -3,5 +3,6 @@ trash_id = 0
 total_rejects = 0
 to_delete = []
 fatigue_multiplier = 0.001
-timeout_multiplier  = 0.5
+timeout_multiplier  = 0
 cells = (12, 14, 17, 19, 21)
+enum_cells = dict()
